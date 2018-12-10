ab05169423ba2039b595e978479d5ce646efacdd
Sahib <sahib.sethi30@gmail.com>
2018-12-10 09:09:26 +0530

    adding preorder and post order traversal

--- trees/printTree.py
+++ trees/printTree.py
@@ -4,3 +4,20 @@ def printTree(node):
     printTree(node.left)
     print(node.data)
     printTree(node.right)
+
+def preOrder(node):
+	if not node:
+		return
+
+	print(node.data)
+	preOrder(node.left)
+	preOrder(node.right)
+
+
+def postOrder(node):
+	if not node:
+		return
+
+	postOrder(node.left)
+	postOrder(node.right)
+	print(node.data)
---
+++ trees/showTree.py
@@ -0,0 +1,6 @@
+from createTree import *
+from printTree import *
+
+root = createTree()
+
+postOrder(root)
